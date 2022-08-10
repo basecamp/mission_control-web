745d0208df0d9a749e949e6073883f3dcb73f2bd
Lewis Buckley <lewis@basecamp.com>
2022-08-10 09:26:03 +0100

    Expand acceptable performance overhead

--- test/performance/mission_control/web/request_filter_middleware_performance_test.rb
+++ test/performance/mission_control/web/request_filter_middleware_performance_test.rb
@@ -2,14 +2,14 @@ require "test_helper"
 
 module MissionControl::Web
   class RequestFilterMiddlewarePerformanceTest < ActionDispatch::IntegrationTest
-    test "performance overhead with middlware enabled is less than 30%" do
+    test "performance overhead with middlware enabled is less than 35%" do
       baseline = -> {
         MissionControl::Web.configuration.enabled = false
 
         get posts_path
       }
 
-      assert_slower_by_at_most 1.3, baseline: baseline do
+      assert_slower_by_at_most 1.35, baseline: baseline do
         MissionControl::Web.configuration.enabled = true
 
         get posts_path
