fb0f76cf829e0668c47b7d9867fd1f014d2cc2ef
Lewis Buckley <lewis@basecamp.com>
2022-08-15 10:07:29 +0100

    Add a memory test warmup to reduce flakyness

--- test/performance/mission_control/web/request_filter_middleware_performance_test.rb
+++ test/performance/mission_control/web/request_filter_middleware_performance_test.rb
@@ -20,8 +20,10 @@ module MissionControl::Web
       end
     end
 
-    test "memory overhead with middleware enabled is less than 55%" do
-      get posts_path # warmup
+    test "memory overhead with middleware enabled is less than 55%" do\
+      warmup = -> {
+        get posts_path
+      }
 
       baseline = -> {
         MissionControl::Web.configuration.enabled = false
@@ -31,7 +33,7 @@ module MissionControl::Web
         assert_equal 200, status
       }
 
-      assert_uses_more_memory_by_at_most 1.55, baseline: baseline do
+      assert_uses_more_memory_by_at_most 1.55, warmup: warmup, baseline: baseline do
         MissionControl::Web.configuration.enabled = true
 
         get posts_path
