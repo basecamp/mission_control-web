8a2b1dd00192a304c9ee23d16a6d9bab62d147cb
Lewis Buckley <lewis@basecamp.com>
2022-08-18 11:23:58 +0100

    Refine layout and terminology

--- app/helpers/mission_control/web/routes_helper.rb
+++ app/helpers/mission_control/web/routes_helper.rb
@@ -1,9 +1,9 @@
 module MissionControl::Web::RoutesHelper
   def route_status_tag(enabled)
     if enabled
-      tag.span "Enabled", class: "tag is-success"
+      tag.span "Allowed", class: "tag is-success"
     else
-      tag.span "Disabled", class: "tag is-danger"
+      tag.span "Denied", class: "tag is-danger"
     end
   end
 end
