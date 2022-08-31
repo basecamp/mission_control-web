363e57b949791fc865cd98eaff8cbd74ac3c75e0
Lewis Buckley <lewis@basecamp.com>
2022-08-31 15:34:22 +0100

    Test that Redis calls are cached

--- test/mission_control/web/routes_cache_test.rb
+++ test/mission_control/web/routes_cache_test.rb
@@ -18,4 +18,27 @@ class MissionControl::Web::RoutesCacheTest < ActiveSupport::TestCase
 
     assert_not @routes.disabled?("/posts/123")
   end
+
+  test "Redis access is cached with a TTL" do
+    @@smembers_call_count = 0
+
+    original_cache_ttl = MissionControl::Web.configuration.cache_ttl
+    MissionControl::Web.configuration.cache_ttl = 10.seconds
+    MissionControl::Web.configuration.redis = Class.new do
+      def smembers(key)
+        @@smembers_call_count += 1
+        []
+      end
+
+      def flushdb; end
+    end.new
+
+    100.times do
+      @routes.disabled?("/posts/123")
+    end
+
+    assert_equal 1, @@smembers_call_count
+
+    MissionControl::Web.configuration.cache_ttl = original_cache_ttl
+  end
 end
