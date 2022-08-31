899c421f836366b36e7577a8bc3c757fa79a83ff
Lewis Buckley <lewis@basecamp.com>
2022-08-31 15:04:15 +0100

    Make RoutesCache#add private

--- lib/mission_control/web/routes_cache.rb
+++ lib/mission_control/web/routes_cache.rb
@@ -9,19 +9,19 @@ class MissionControl::Web::RoutesCache
     end
   end
 
-  def add(route)
-    MissionControl::Web.redis.sadd REDIS_KEY, route.pattern.to_s
+  def disabled?(path)
+    all_disabled_patterns.any? { |pattern| Regexp.new(pattern) =~ path }
   end
 
   def remove(route)
     MissionControl::Web.redis.srem REDIS_KEY, route.pattern.to_s
   end
 
-  def disabled?(path)
-    all_disabled_patterns.any? { |pattern| Regexp.new(pattern) =~ path }
-  end
-
   private
+    def add(route)
+      MissionControl::Web.redis.sadd REDIS_KEY, route.pattern.to_s
+    end
+
     def all_disabled_patterns
       # Using Redis client rather than Kredis as request interception with a middlware is performance-critical.
       MissionControl::Web.redis.smembers REDIS_KEY
