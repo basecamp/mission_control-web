0370abdd7ce4c86d1227613025b7527f62fb297b
Lewis Buckley <lewis@basecamp.com>
2022-08-31 15:38:01 +0100

    Test that the TTL is respected

--- test/mission_control/web/routes_cache_test.rb
+++ test/mission_control/web/routes_cache_test.rb
@@ -20,25 +20,41 @@ class MissionControl::Web::RoutesCacheTest < ActiveSupport::TestCase
   end
 
   test "Redis access is cached with a TTL" do
-    @@smembers_call_count = 0
-
     original_cache_ttl = MissionControl::Web.configuration.cache_ttl
     MissionControl::Web.configuration.cache_ttl = 10.seconds
-    MissionControl::Web.configuration.redis = Class.new do
-      def smembers(key)
-        @@smembers_call_count += 1
-        []
-      end
-
-      def flushdb; end
-    end.new
+    MissionControl::Web.configuration.redis = fake_redis = FakeRedisWithCallCounter.new
 
     100.times do
       @routes.disabled?("/posts/123")
     end
 
-    assert_equal 1, @@smembers_call_count
+    assert_equal 1, fake_redis.smembers_call_count
 
     MissionControl::Web.configuration.cache_ttl = original_cache_ttl
   end
+
+  test "the TTL is respected" do
+    original_cache_ttl = MissionControl::Web.configuration.cache_ttl
+    MissionControl::Web.configuration.cache_ttl = 10.seconds
+    MissionControl::Web.configuration.redis = fake_redis = FakeRedisWithCallCounter.new
+
+    @routes.disabled?("/posts/123")
+    travel 11.seconds
+    @routes.disabled?("/posts/123")
+
+    assert_equal 2, fake_redis.smembers_call_count
+
+    MissionControl::Web.configuration.cache_ttl = original_cache_ttl
+  end
+end
+
+class FakeRedisWithCallCounter
+  class_attribute :smembers_call_count, default: 0
+
+  def smembers(key)
+    self.smembers_call_count += 1
+    []
+  end
+
+  def flushdb; end
 end
