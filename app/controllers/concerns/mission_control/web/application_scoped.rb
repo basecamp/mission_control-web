c3deda0d665f645570dc99c2101de7cb867ce365
Lewis Buckley <lewis@basecamp.com>
2022-09-02 10:37:04 +0100

    Rename ensure_application_param_id to make intention clearer

--- app/controllers/concerns/mission_control/web/application_scoped.rb
+++ app/controllers/concerns/mission_control/web/application_scoped.rb
@@ -2,12 +2,12 @@ module MissionControl::Web::ApplicationScoped
   extend ActiveSupport::Concern
 
   included do
-    before_action :ensure_application_id_param
+    before_action :ensure_application_scope
     before_action :set_application
   end
 
   private
-    def ensure_application_id_param
+    def ensure_application_scope
       redirect_to application_routes_path(MissionControl::Web::Application.first) unless params[:application_id]
     end
 
