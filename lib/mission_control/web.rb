1fa683aab978d38d7376f3f8276af01765932244
Lewis Buckley <lewis@basecamp.com>
2022-09-05 16:36:45 +0100

    Move requires to top

--- lib/mission_control/web.rb
+++ lib/mission_control/web.rb
@@ -1,11 +1,11 @@
+require "mission_control/web/engine"
+require "active_support/core_ext/module/attribute_accessors_per_thread"
+
 require "zeitwerk"
 loader = Zeitwerk::Loader.new
 loader.inflector = Zeitwerk::GemInflector.new(__FILE__)
 loader.push_dir(File.expand_path("..", __dir__))
 loader.setup
-
-require "mission_control/web/engine"
-require "active_support/core_ext/module/attribute_accessors_per_thread"
 
 module MissionControl
   module Web
