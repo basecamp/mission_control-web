554df44e0b51fcb80663aecc7b6d648abea842e6
Lewis Buckley <lewis@basecamp.com>
2022-09-06 13:31:49 +0100

    Rename configuration.enabled to configuration.middleware_enabled
    Helps to better clarify the purpose of the flag

--- lib/mission_control/web/configuration.rb
+++ lib/mission_control/web/configuration.rb
@@ -1,15 +1,13 @@
 class MissionControl::Web::Configuration
   include ActiveModel::Attributes, ActiveModel::Dirty
 
-  attribute :enabled,               :boolean, default: true
+  attribute :middleware_enabled,    :boolean, default: true
   attribute :routes_cache_ttl,      :integer, default: 10.seconds
   attribute :base_controller_class, :string,  default: "::ApplicationController"
 
   attr_writer :redis, :administered_applications
 
-  def disabled?
-    !enabled
-  end
+  alias :middleware_enabled? :middleware_enabled
 
   def redis
     @redis || raise("Redis client not configured. Configure with MissionControl::Web.configuration.redis = Redis.new")
