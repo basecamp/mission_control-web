04f3d267cfb9986678290c15c9669185ee679f7b
Lewis Buckley <lewis@basecamp.com>
2022-10-25 10:24:24 +0100

    Scope route pattern uniqueness check

--- app/models/mission_control/web/route.rb
+++ app/models/mission_control/web/route.rb
@@ -2,7 +2,7 @@ class MissionControl::Web::Route < ApplicationRecord
   include Applications
 
   validates :name, :pattern, presence: true
-  validates :pattern, uniqueness: true
+  validates :pattern, uniqueness: { scope: :application_id }
 
   def application
     @application ||= MissionControl::Web::Application.find!(application_id)
